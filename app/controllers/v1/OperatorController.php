1b83a34e9f8e02b22cd5787a16e973ec5c395da9
rafulll <raful@aluno.fapce.edu.br>
2019-12-03 19:17:39 -0300

    Ajustes semi-finais

--- app/controllers/v1/OperatorController.php
+++ app/controllers/v1/OperatorController.php
@@ -8,22 +8,98 @@ use Psr\Http\Message\ResponseInterface as Response;
 final class OperatorController
 {
 
-    /**
-     * @api {GET} /status Status da API
-     * 
-     * @apiVersion 1.0.0
-     * 
-     * @apiDescription Verifica a disponibilidade da API
-     * 
-     * @apiGroup Recursos Abertos
-     * 
-     * @apiSuccess (200) {String} status Resultado da disponibilidade do servidor.
-     * 
-     * @apiSuccessExample {JSON} Success-Response:
-     *  {
-     *      'status': 'Serviço disponível WS1'
-     *  }
-     */
+  /** 
+   * @api {GET} /ws-operators/v1/status Operators - Status da API
+   * @apiVersion 0.0.1
+   * @apiDescription Verifica a disponibilidade da API
+   * @apiGroup Recursos Abertos
+   * 
+   * @apiSuccess {String} status Resultado da disponibilidade do servidor.
+   * 
+   * @apiSuccessExample [JSON] Success-Response
+   * {
+   *   'status': 'Serviço disponível WS1'
+   * }
+   *  
+  */
+
+  /**
+   * @api {POST} /ws-operators/v1/pay/:operadora Operators - Executa o pagamento via cartão de crédito
+   * @apiVersion 0.0.1
+   * @apiDescription Envia a solicitação para pagamento via cartão de crédito a operadora de cartão.
+   * @apiGroup Recursos Autenticados
+   * 
+   * @apiParam {String} operadora Parâmetro da url que corresponde a operadora de cartão desejado. Deve ser uma das opções a seguir: op-01, op-02 ou op-03.
+   * @apiParam {String} numero_cartao Número do cartão de crédito.
+   * @apiParam {String} nome_cliente Nome do titular do cartão de crédito.
+   * @apiParam {String} bandeira Nome da bandeira segundo opções a seguir: mister (cod.: 1111), vista (cod.: 2222) ou daciolo (cod.: 3333).
+   * @apiParam {Number} cod_seguranca Código de três dígitos.
+   * @apiParam {Number} valor_em_centavos Valor em centavos da compra.
+   * @apiParam {Number} parcelas Quantidade de parcelas para o pagamento.
+   * @apiParam {String} cod_loja Código único da loja e-commerce. Será usado para que a operadora de cartão verifique se a loja é sua cliente. Deve ser uma das opções a seguir: loja-01, loja-02 ou loja-03.
+   * 
+   * @apiParamExample [JSON] Exemplo Corpo da Requisição
+   * {
+   *   "numero_cartao": "1111.2222.3333.4444",
+   *   "nome_cliente": "USUARIO DA SILVA",
+   *   "bandeira": "mister",
+   *   "cod_seguranca": 111,
+   *   "valor_em_centavos": 500,
+   *   "parcelas": 12,
+   *   "cod_loja": "loja-xx"
+   * }
+   * 
+   * @apiSuccess {String} resposta Resultado da transação.
+   * @apiSuccess {String} nome_cliente Nome do titular do cartão de crédito.
+   * @apiSuccess {Number} valor_em_centavos Valor em centavos da compra.
+   * @apiSuccess {Number} parcelas Quantidade de parcelas em que o pagamento foi feito.
+   * 
+   * @apiSuccessExample [JSON] Exemplo-Resposta-Sucesso
+   * {
+   *   "resposta": "sucesso",
+   *   "nome_cliente": "USUARIO DE SOUSA",
+   *   "valor_em_centavos": 500,
+   *   "parcelas": 12
+   * }
+   * 
+   * @apiError (400) {String} resposta Resultado da transação.
+   * @apiError (400) {String} detalhes Detalhes do erro
+   * @apiError (400) {String} operadora Operadora que foi buscado.
+   * @apiError (400) {String} cod-loja Loja de onde partiu a compra.
+   * @apiError (400) {String} bandeira Bandeira requisitada.
+   * @apiError (400) {String} parcelas_solicitadas Quantidade de parcelas solicitadas.
+   * @apiError (400) {String} limite_parcelas Limite de parcelas da bandeira.
+   * 
+   * @apiErrorExample [JSON] Resposta-Erro-Operadora-Inexistente
+   * {
+   *   "resposta": "falha",
+   *   "detalhes": "Operadora não existe",
+   *   "operadora": "op-xx"
+   * }
+   * @apiErrorExample [JSON] Resposta-Erro-Loja-Negada
+   * {
+   *   "resposta": "falha",
+   *   "detalhes": "Loja não autorizada",
+   *   "operadora": "op-xx",
+   *   "cod-loja": "loja-xx"
+   * }
+   * @apiErrorExample [JSON] Resposta-Erro-Bandeira-Negada
+   * {
+   *   "resposta": "falha",
+   *   "detalhes": "Bandeira não autorizada",
+   *   "operadora": "op-xx",
+   *   "bandeira": "mister"
+   * }
+   * @apiErrorExample [JSON] Resposta-Erro-Parcelas-Não-Aceitas
+   * {
+   *   "resposta": "falha",
+   *   "detalhes": "Limite de parcelas ultrapassado",
+   *   "bandeira": "mister"
+   *   "parcelas_solicitadas": 00,
+   *   "limite_parcelas": 00
+   * }
+   *  
+  */
     public static function getStatus(Request $req, Response $res, array $args)
     {
         $dados = [
@@ -94,9 +170,9 @@ final class OperatorController
 
         } else {
                 //var_dump($vdata[0][$args['operator']]['bandeiras_autorizadas']);
-             
+                //echo "vai pedir brands";
       
-                $url="http://localhost/ws-brands/v1/pay";
+                $url="http://localhost/ws-brands/v1/installments-limit/".$data[0]['bandeira'];
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL,$url);
                 curl_setopt($ch, CURLOPT_POST, 1);
@@ -112,7 +188,7 @@ final class OperatorController
                     JSON_ERROR_SYNTAX => 'Yes,_Syntax error',
                 );
                 
-                //echo 'Json_errors: __ ', $json_errors[json_last_error()], PHP_EOL, PHP_EOL;
+                echo 'Json_errors: __ ', $json_errors[json_last_error()], PHP_EOL, PHP_EOL;
                 //var_dump($c);
                         return $rs->withStatus(200)->withJson($c);
                               
--- docs/api_data.js
+++ docs/api_data.js
@@ -1,16 +1,16 @@
 define({ "api": [
   {
     "type": "GET",
-    "url": "/status",
-    "title": "Status da API",
-    "version": "1.0.0",
+    "url": "/ws-operators/v1/status",
+    "title": "Operators - Status da API",
+    "version": "0.0.1",
     "description": "<p>Verifica a disponibilidade da API</p>",
     "group": "Recursos_Abertos",
     "success": {
       "fields": {
-        "200": [
+        "Success 200": [
           {
-            "group": "200",
+            "group": "Success 200",
             "type": "String",
             "optional": false,
             "field": "status",
@@ -20,15 +20,15 @@ define({ "api": [
       },
       "examples": [
         {
-          "title": "Success-Response:",
-          "content": "{\n    'status': 'Serviço disponível WS1'\n}",
-          "type": "JSON"
+          "title": "[JSON] Success-Response",
+          "content": "{\n  'status': 'Serviço disponível WS1'\n}",
+          "type": "json"
         }
       ]
     },
     "filename": "app/controllers/v1/OperatorController.php",
     "groupTitle": "Recursos_Abertos",
-    "name": "GetStatus"
+    "name": "GetWsOperatorsV1Status"
   },
   {
     "type": "POST",
@@ -193,5 +193,203 @@ define({ "api": [
     "filename": "app/controllers/v1/OperatorController.php",
     "groupTitle": "Recursos_Autenticados",
     "name": "PostPayOperator"
+  },
+  {
+    "type": "POST",
+    "url": "/ws-operators/v1/pay/:operadora",
+    "title": "Operators - Executa o pagamento via cartão de crédito",
+    "version": "0.0.1",
+    "description": "<p>Envia a solicitação para pagamento via cartão de crédito a operadora de cartão.</p>",
+    "group": "Recursos_Autenticados",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "operadora",
+            "description": "<p>Parâmetro da url que corresponde a operadora de cartão desejado. Deve ser uma das opções a seguir: op-01, op-02 ou op-03.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "numero_cartao",
+            "description": "<p>Número do cartão de crédito.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "nome_cliente",
+            "description": "<p>Nome do titular do cartão de crédito.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "bandeira",
+            "description": "<p>Nome da bandeira segundo opções a seguir: mister (cod.: 1111), vista (cod.: 2222) ou daciolo (cod.: 3333).</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "cod_seguranca",
+            "description": "<p>Código de três dígitos.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "valor_em_centavos",
+            "description": "<p>Valor em centavos da compra.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "parcelas",
+            "description": "<p>Quantidade de parcelas para o pagamento.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "cod_loja",
+            "description": "<p>Código único da loja e-commerce. Será usado para que a operadora de cartão verifique se a loja é sua cliente. Deve ser uma das opções a seguir: loja-01, loja-02 ou loja-03.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "[JSON] Exemplo Corpo da Requisição",
+          "content": "{\n  \"numero_cartao\": \"1111.2222.3333.4444\",\n  \"nome_cliente\": \"USUARIO DA SILVA\",\n  \"bandeira\": \"mister\",\n  \"cod_seguranca\": 111,\n  \"valor_em_centavos\": 500,\n  \"parcelas\": 12,\n  \"cod_loja\": \"loja-xx\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "resposta",
+            "description": "<p>Resultado da transação.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "nome_cliente",
+            "description": "<p>Nome do titular do cartão de crédito.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "valor_em_centavos",
+            "description": "<p>Valor em centavos da compra.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "parcelas",
+            "description": "<p>Quantidade de parcelas em que o pagamento foi feito.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "[JSON] Exemplo-Resposta-Sucesso",
+          "content": "{\n  \"resposta\": \"sucesso\",\n  \"nome_cliente\": \"USUARIO DE SOUSA\",\n  \"valor_em_centavos\": 500,\n  \"parcelas\": 12\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "fields": {
+        "400": [
+          {
+            "group": "400",
+            "type": "String",
+            "optional": false,
+            "field": "resposta",
+            "description": "<p>Resultado da transação.</p>"
+          },
+          {
+            "group": "400",
+            "type": "String",
+            "optional": false,
+            "field": "detalhes",
+            "description": "<p>Detalhes do erro</p>"
+          },
+          {
+            "group": "400",
+            "type": "String",
+            "optional": false,
+            "field": "operadora",
+            "description": "<p>Operadora que foi buscado.</p>"
+          },
+          {
+            "group": "400",
+            "type": "String",
+            "optional": false,
+            "field": "cod-loja",
+            "description": "<p>Loja de onde partiu a compra.</p>"
+          },
+          {
+            "group": "400",
+            "type": "String",
+            "optional": false,
+            "field": "bandeira",
+            "description": "<p>Bandeira requisitada.</p>"
+          },
+          {
+            "group": "400",
+            "type": "String",
+            "optional": false,
+            "field": "parcelas_solicitadas",
+            "description": "<p>Quantidade de parcelas solicitadas.</p>"
+          },
+          {
+            "group": "400",
+            "type": "String",
+            "optional": false,
+            "field": "limite_parcelas",
+            "description": "<p>Limite de parcelas da bandeira.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "[JSON] Resposta-Erro-Operadora-Inexistente",
+          "content": "{\n  \"resposta\": \"falha\",\n  \"detalhes\": \"Operadora não existe\",\n  \"operadora\": \"op-xx\"\n}",
+          "type": "json"
+        },
+        {
+          "title": "[JSON] Resposta-Erro-Loja-Negada",
+          "content": "{\n  \"resposta\": \"falha\",\n  \"detalhes\": \"Loja não autorizada\",\n  \"operadora\": \"op-xx\",\n  \"cod-loja\": \"loja-xx\"\n}",
+          "type": "json"
+        },
+        {
+          "title": "[JSON] Resposta-Erro-Bandeira-Negada",
+          "content": "{\n  \"resposta\": \"falha\",\n  \"detalhes\": \"Bandeira não autorizada\",\n  \"operadora\": \"op-xx\",\n  \"bandeira\": \"mister\"\n}",
+          "type": "json"
+        },
+        {
+          "title": "[JSON] Resposta-Erro-Parcelas-Não-Aceitas",
+          "content": "{\n  \"resposta\": \"falha\",\n  \"detalhes\": \"Limite de parcelas ultrapassado\",\n  \"bandeira\": \"mister\"\n  \"parcelas_solicitadas\": 00,\n  \"limite_parcelas\": 00\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "app/controllers/v1/OperatorController.php",
+    "groupTitle": "Recursos_Autenticados",
+    "name": "PostWsOperatorsV1PayOperadora"
   }
 ] });
